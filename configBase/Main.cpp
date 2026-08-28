5e0393079b26da971278112b1ef9a2f8969deccc
Rodrigo Uribe <rodrigouribe255@gmail.com>
2026-08-27 18:55:47 -0600

    Segundo commit

--- configBase/Main.cpp
+++ configBase/Main.cpp
@@ -24,7 +24,7 @@ const GLchar* fragmentShaderSource =
 	"out vec4 color;\n"
 	"void main()\n"
 	"{\n"
-	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"color = vec4(0.0f, 0.1f, 0.3f, 1.0f);\n"
 	"}\n\0"
 };
 
@@ -79,7 +79,7 @@ int main() {
 	// Define las dimensiones del viewport
 	glViewport(0, 0, screenWidth, screenHeight);
 
-	//CrearShader();
+	CrearShader();
 
 	// Set up vertex data (and buffer(s)) and attribute pointers
 	GLfloat vertices[] =
@@ -119,7 +119,7 @@ int main() {
 
 		// Render
 		// Clear the colorbuffer
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.3f, 0.2f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 
